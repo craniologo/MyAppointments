ad63c61d9f05c274adcd9bb43c164fdbc56dde3f
Edgar Abanto <32318892+craniologo@users.noreply.github.com>
2022-09-01 06:55:34 -0500

    Add snackBar and override fun onBackPressed

--- app/src/main/java/com/sergestec/myappointments/MainActivity.kt
+++ app/src/main/java/com/sergestec/myappointments/MainActivity.kt
@@ -5,11 +5,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import com.sergestec.myappointments.PreferenceHelper.get
 import com.sergestec.myappointments.PreferenceHelper.set
 
 class MainActivity : AppCompatActivity() {
+
+    private val snackBar by lazy {
+        Snackbar.make(mainLayout, R.string.press_back_again, Snackbar.LENGTH_SHORT)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,10 +24,6 @@ class MainActivity : AppCompatActivity() {
             //  SQLite
             //  files
 
-        /*
-        val preferences = getSharedPreferences ( "general", Context.MODE_PRIVATE)
-        val session = preferences.getBoolean("session", false)
-         */
         val preferences = PreferenceHelper.defaultPrefs(this)
         if (preferences["session", false])
             goToMenuActivity()
@@ -41,12 +43,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createSessionPreference() {
-        /*
-        val preferences = getSharedPreferences ( "general", Context.MODE_PRIVATE)
-        val editor = preferences.edit()
-        editor.putBoolean("session", true)
-        editor.apply()
-         */
         val preferences = PreferenceHelper.defaultPrefs(this)
         preferences["session"] = true
     }
@@ -55,5 +51,12 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, MenuActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    override fun onBackPressed() {
+        if (snackBar.isShown)
+            super.onBackPressed()
+        else
+            snackBar.show()
     }
 }
